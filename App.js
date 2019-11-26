ab6026910f7e3247820540463eb714e044fa105f
Alexispoveda <alexis.mars20@hotmail.com>
2019-11-26 17:40:45 -0500

    Little error

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import * as Device from 'expo-device';
 
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
@@ -9,7 +8,7 @@ export default function App() {
   return (
     <View style={styles.screen}>
         <Header title={"Guess a number"}/>
-        s<StartGameScreen/>
+        <StartGameScreen/>
     </View>
   );
 }
